619ab7fad3c6cb5f239bb536966db06f59989903
divijdawar <2divijdawar@gmail.com>
2025-05-28 23:22:02 +0800

    Added file

--- gemma3_inference/layer_norm.cu
+++ gemma3_inference/layer_norm.cu
@@ -1,13 +1,15 @@
 #include <cuda_runtime.h> 
 #include <torch/extension.h>
 
-template <typename T, typnename Accum=float>
-__global__ void layer_norm_kernel(
-    const T *__restrict__ input, 
-    T *__restrict__ output, 
-    const T *__restrict__ gamma, 
-    const T *__restrict__ beta, 
+template <typename T>
+__global__ void __launch_bounds__(MAX_THREADS_PER_BLOCK, MIN_BLOCKS_PER_SM) layer_norm_kernel(
+    const T* __restrict__ input,
+          T* __restrict__ output, 
+    const T* __restrict__ gamma, // scale parameter
+    const T* __restrict__ beta, // shift parameter
     int N, 
     int D, 
-
-)
+    float epsilon
+) { 
+    int thread_idx = 
+}
