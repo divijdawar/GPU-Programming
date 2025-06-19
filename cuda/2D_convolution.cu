740c4294d6663b47396607a8b22896b60de60d9b
divijdawar <2divijdawar@gmail.com>
2025-06-19 13:19:27 +0530

    2d convolution kernel v0

--- cuda/2D_convolution.cu
+++ cuda/2D_convolution.cu
@@ -1,9 +1,9 @@
 #include <cuda_runtime.h>
 
-void convolution2D(
-    const float* input, 
-    const float* kernel, 
-    float* output, 
+__global__ void convolution2D(
+    const float* __restrict__ input, 
+    const float* __restrict__ kernel, 
+    float* __restrict__ output, 
     int input_rows, 
     int input_cols, 
     int kernel_rows, 
@@ -12,8 +12,8 @@ void convolution2D(
     int output_rows = input_rows - kernel_rows + 1; 
     int output_cols = input_cols - kernel_cols + 1; 
 
-    int row = blockIdx.y + block_dim.y + threadIdx.y 
-    int col = blockIdx.x + block_dim.x + threadIdx.x 
+    int row = blockIdx.y * blockDim.y + threadIdx.y; 
+    int col = blockIdx.x * blockDim.x + threadIdx.x; 
 
     if (row >= output_rows || col >= output_cols) return; 
 
@@ -21,6 +21,8 @@ void convolution2D(
 
     for(int i = 0; i < kernel_rows; i++) { 
         for (int j = 0; j < kernel_cols; j++) { 
+            accum += input[(row + i) * input_cols + (col + j)] * kernel[i * kernel_cols + j]; 
         }
     }
+    output[row * output_cols + col] = accum; 
 }
