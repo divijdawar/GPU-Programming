c41a5fa8298a7616e1b3aed449d365996aa7b714
divijdawar <2divijdawar@gmail.com>
2025-06-20 23:03:27 +0530

    qkv v0

--- gemma3_inference/qkv_projection.cu
+++ gemma3_inference/qkv_projection.cu
@@ -4,17 +4,20 @@
 
 #define MAX_THREADS_PER_BLOCK 1024
 #define MIN_BLOCKS_PER_SM 1
+#define TILE_SIZE 16
 
 __global__ void __launch_bounds__(MAX_THREADS_PER_BLOCK, MIN_BLOCKS_PER_SM) qkv_projection_kernel(
-    const half* __restrict__ input, // [B,T, D]
-    const half* __restrict__ W_q, // [B, 3D]
-          half* __restrict__ output, // [B, T, 3D]
+    const half* __restrict__ X, // [B,T, D]
+          half* __restrict__ output, // [B, T, 3H]
+    const half* __restrict__ W_q, // [3H, D]
     int B, // batch size
-    int T, // sequence length
+    int T, // sequence length 
     int D, // hidden dimension
-    int D_q, // query dimension
-    int D_k, // key dimension
-    int D_v // value dimension
-) {
+    int H // number of heads
+) { 
+    int b = blockIdx.x; // batch index
+    int t = blockIdx.y; // sequence index
+    int h = blockIdx.z; // hidden dimension index
     
+    int idx = 
 }
