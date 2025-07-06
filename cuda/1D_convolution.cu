dbf3a63aaef5f9bc398bbaa053a152787352db1d
divijdawar <2divijdawar@gmail.com>
2025-07-06 19:02:54 +0530

    added 1d convolution

---
+++ cuda/1D_convolution.cu
@@ -0,0 +1,36 @@
+#include <cuda_runtime.h> 
+
+__constant__ float kernel[3]; 
+
+__global__ void convolution1D(
+    const float* input, 
+    const float* kernel, 
+    const float* output, 
+    int input_size, 
+    int kernel_size) { 
+
+        __shared__ float shared_input[]; 
+
+        int output_size = input_size - kernel_size + 1; 
+        int t = blockIdx.x * blockDim.x + threadIdx.x; 
+        int tid = threadIdx.x; 
+        int offset = blockIdx.x * blockDim.x; 
+        int tile_size = blockDim.x + kernel_size - 1; 
+
+        if (tid < tile_size && (offset + tid) < input_size) { 
+            shared_input[tid] = input[offset + tid]; 
+        }
+        __syncthreads();
+
+        if (t >= output_size) return; 
+
+        float accum = 0.0f; 
+
+        for (int i = 0; i < kernel_size; i++) { 
+            accum += shared_input[t + i] * kernel[i]; 
+        }
+
+        output[t] = accum; 
+        __syncthreads(); 
+
+    }
